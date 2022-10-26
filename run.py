790aa7ba906268887856465949bb56fe8e46b9d3
David Abraham <davidabrahamy10@gmail.com>
2022-10-26 18:56:50 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 import gspread # This imports the entire gspread library so we can access any function or method within it
 from google.oauth2.service_account import Credentials # This imports the credentials class which is part of the google auth library. We only need the Credentials class so we don't have to import the entire library.
 # Now that we have our library the next thing is to set our SCOPE.
@@ -16,18 +12,30 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
+
+
+
 def get_sales_data():
     """
-    Get sales figure input from the user
+    Get sales figures input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")  
+        data_str = input("Enter your data here: ")  
 
-    sales_data = data_str.split(",") # The split method returns the data taken from data_str as a list. In order to insert the data into our spreadsheet the values need to first be in a list.
-    validate_data(sales_data)
+        sales_data = data_str.split(",") # The split method returns the data taken from data_str as a list. In order to insert the data into our spreadsheet the values need to first be in a list.
+        
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
+
+
 
 def validate_data(values):
     """ 
@@ -43,6 +51,8 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-    
+        return False
 
-get_sales_data()
+    return True
+
+data = get_sales_data()
